964aedfcf3085404d87eeb2f6ee5ea6b39bc67a7
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2024-01-07 15:55:16 -0300

    updates

---
+++ js/etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulado de tal',
+                partido: 'ABC',
+                foto: [
+                    {url: '/images/putin.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                patido: 'DEFG',
+                fotos: [
+                    {url:'/images/tarci.png', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'Prefeito',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cicero',
+                fotos: [
+                    {url: '/images/bolso.png', legenda: 'Prefeito'},
+                    {url: '/images/trump.png', legenda: 'Vice-Prefeiro', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'DSA',
+                vice: 'Matheus',
+                fotos: [
+                    {url: '/images/lula.png', legenda: 'Prefeito'},
+                    {url: '/images/zelen.png', legenda: 'Vice-Prefeiro', small: true}
+                ] 
+            }
+        ]
+    }];
---
+++ js/script.js
@@ -0,0 +1,10 @@
+let qs = el => document.querySelector(el);
+
+let seuVotoPara = qs('.d-1-1 span');
+let cargo = qs('.d-1-2 span');
+let descricao = qs('.d-1-4');
+let aviso = qs('.d-2');
+let lateral = qs('.d-1--right');
+let numeros = qs('.d-1-3');
+
+
